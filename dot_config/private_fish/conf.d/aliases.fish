31241f352a04b01a007940faf4f41be8c62371a1
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-08-14 23:00:06 +1000

    Rearrange aliases

---
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -0,0 +1,5 @@
+alias py=python3
+alias omp=oh-my-posh
+alias ptp=ptpython
+alias pti=ptipython
+alias cz='chezmoi'
--- dot_config/private_fish/conf.d/python_venv.fish
+++ dot_config/private_fish/conf.d/python_venv.fish
@@ -1,3 +1,4 @@
 # Python venv
 alias v='source .venv/bin/activate.fish'
 alias vv='..; v; prevd'
+alias dv=deactivate
