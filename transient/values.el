4ed2717fe926801a4d79f727523e80f535640923
Vincent Ging Ho Yim <28189684+cenviity@users.noreply.github.com>
2024-02-27 00:48:13 +1100

    Use `--no-ff` setting for `git merge` by default

--- transient/values.el
+++ transient/values.el
@@ -1,1 +1,2 @@
-((magit-log:magit-log-mode "-n256" "--date-order" "--graph" "--color" "--decorate"))
+((magit-log:magit-log-mode "-n256" "--date-order" "--graph" "--color" "--decorate")
+ (magit-merge "--no-ff"))
