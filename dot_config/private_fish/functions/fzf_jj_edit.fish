e92440ee9c395ed0f055425aa99831a13b5b86da
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-07-17 23:30:01 +1000

    Revert "Add revision picker for Jujutsu using fzf"
    This reverts commit d7aef89380ea5a6bc0072156fab5b2763df9b02c.

--- dot_config/private_fish/functions/fzf_jj_edit.fish
+++
@@ -1,8 +0,0 @@
-function fzf_jj_edit
-    if test -d .jj
-        set -l name (jj log --no-graph -T 'change_id.shortest() ++ "\t" ++ description.first_line() ++ " "  ++ branches.join("  ") ++ "\n"' --color always | fzf --ansi | cut -f1)
-        commandline -it "$name"
-    else
-        commandline -it "# not in a jj directory"
-    end
-end
